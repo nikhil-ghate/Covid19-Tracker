cb83b071c34b7349ad25172617190b93f2a5762c
Nikhil <nikhilghate19@gmail.com>
2020-07-06 16:32:26 +0530

    new components added

--- js/app.js
+++ js/app.js
@@ -5,13 +5,17 @@ const updateDate = document.getElementById("update");
 const totalCasesElement = document.querySelector(".total-cases .value");
 const newCasesElement = document.querySelector(".total-cases .new-value");
 
+const activeElement = document.querySelector(".active .value");
+const newActiveElement = document.querySelector(".active .new-value")
+
 const recoverdElement = document.querySelector(".recovered .value");
 const newRecoverdElement = document.querySelector(".recovered .new-value");
 
 const deathsElement = document.querySelector(".deaths .value");
 const newDeathsElement = document.querySelector(".deaths .new-value");
 
-const ctx = document.getElementById("axes_line_chart");
+const recoveryElement = document.querySelector(".recovery .value");
+const fatalityElement = document.querySelector(".fatality .value");
 
 //APP VARIABLES
 let appData = [],
@@ -48,12 +52,17 @@ function fetchData(userCountry){
 				
 				totalCasesElement.innerHTML = cnt.TotalConfirmed || 0;
 				newCasesElement.innerHTML = `+${cnt.NewConfirmed || 0}`;
+				activeElement.innerHTML = `${cnt.TotalConfirmed - cnt.TotalRecovered - cnt.TotalDeaths || 0}`;
+				newActiveElement.innerHTML = `+${cnt.NewConfirmed - cnt.NewRecovered - cnt.NewDeaths || 0}`;
 				
 				recoverdElement.innerHTML = cnt.TotalRecovered || 0;
 				newRecoverdElement.innerHTML = `+${cnt.NewRecovered || 0}`;
 
 				deathsElement.innerHTML = cnt.TotalDeaths || 0;
 				newDeathsElement.innerHTML = `+${cnt.NewDeaths || 0}`;
+				// console.log(((cnt.TotalRecovered / cnt.TotalConfirmed) * 100).toFixed(2));
+				recoveryElement.innerHTML = `${((cnt.TotalRecovered / cnt.TotalConfirmed) * 100).toFixed(2) || 0}%`;
+				fatalityElement.innerHTML = `${((cnt.TotalDeaths  / cnt.TotalConfirmed) * 100).toFixed(2) || 0}%`;
 			}
 		})
 
